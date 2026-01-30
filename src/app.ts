86931a107af06ddc2f35c481b316083522a172ea
AtteH <attehiltunen@hotmail.com>
2026-01-30 11:30:24 +0200

    refaktorointi: kutsun error handling siirretty omaan middleware moduuliin

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,12 @@
 // src/app.ts
 import express from "express";
-import { Request, Response, NextFunction } from "express";
 import bookings from "./routes/bookings";
-import { ApiError } from "./utils/errors";
+import { errorHandler } from "./middleware/errorHandler";
 
 export const app = express();
 app.use(express.json());
 
 app.use("/bookings", bookings);
 
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof ApiError) {
-    return res.status(err.status).json({ error: err.message });
-  }
-  console.error(err);
-  res.status(500).json({ error: "Internal server error" });
-});
+app.use(errorHandler);
+
